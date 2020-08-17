d72de2b106284110f41c855f6bae3a5bd8e5ec68
angeloS-98 <69158870+angeloS-98@users.noreply.github.com>
2020-08-17 10:43:07 +0200

    test invio pagine con express (fixare)

--- react-express-app/backend/routes/index.js
+++ react-express-app/backend/routes/index.js
@@ -1,13 +1,28 @@
+var path = require('path');
+var fs = require('fs');
+/*
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from '../../frontend/src/App'
+*/
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  const app = /*ReactDOMServer.renderToString(<App />);*/ 'ciao';
 
-router.get("/registration", function (req, res, next) {
-  res.render("layout", {title: 'Ok'});
+  const indexFile = path.resolve('../../frontend/public/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    );
+  });
 });
 
 module.exports = router;
